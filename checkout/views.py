30cead3d30c1a8dbbe9c87e0e3393fe4229e61fd
Angela-Sin <anzela.sinia@gmail.com>
2025-03-30 18:02:38 +0100

    create strype elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51R8OzcQkbAcKj7Vb1fozqZ8cZ9sGmSjEeNQPWE3soLDGtrCxnrbl1BNhSFGPcegCF1dBAgqwhGTLuRviNySJPgVi00cdZfLsLG',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
